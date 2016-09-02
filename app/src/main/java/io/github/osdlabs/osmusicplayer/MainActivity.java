7504cdc51be0b6ddc34547693477e06a939748d5
yash sharan <yashsharan.0805@gmail.com>
2016-09-02 23:07:48 +0530

    added storage permissions

--- app/src/main/java/io/github/osdlabs/osmusicplayer/MainActivity.java
+++ app/src/main/java/io/github/osdlabs/osmusicplayer/MainActivity.java
@@ -1,16 +1,23 @@
 package io.github.osdlabs.osmusicplayer;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -26,10 +33,14 @@ public class MainActivity extends AppCompatActivity
     ViewPagerAdapter adapter;
     ViewPager viewPager;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
 
         //Reference views
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -56,7 +67,28 @@ public class MainActivity extends AppCompatActivity
             }
         });
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
+
     }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        List<Fragment> fragments = getSupportFragmentManager().getFragments();
+        if (fragments != null) {
+            for (Fragment fragment : fragments) {
+                fragment.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
+    }
+
+
+
 
     //Setting up the View Pager with the fragments
     private void setupViewPager(ViewPager viewPager) {
--- app/src/main/java/io/github/osdlabs/osmusicplayer/MainSongsFragment.java
+++ app/src/main/java/io/github/osdlabs/osmusicplayer/MainSongsFragment.java
@@ -1,19 +1,28 @@
 package io.github.osdlabs.osmusicplayer;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +38,11 @@ public class MainSongsFragment extends Fragment {
     MainSongsAdapter adapter;
     List<MainSongsItemFormat> mainSongsItemFormatList = new ArrayList<>();
 
+    private static final int REQUEST_WRITE_STORAGE = 21;
+    int permissionCheck;
+    int currentApi;
+
+
     public MainSongsFragment() {
     }
 
@@ -43,57 +57,112 @@ public class MainSongsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main_songs, container, false);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.fragment_main_songs_rv);
+        permissionCheck = ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        currentApi = Build.VERSION.SDK_INT;
 
-        populateList();
+
         adapter = new MainSongsAdapter(getContext(), mainSongsItemFormatList);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
+        if (currentApi < Build.VERSION_CODES.M) {
+            populateList();
+            adapter.notifyDataSetChanged();
+        } else
+            getPermission();
 
 
         return view;
     }
 
-    private void populateList() {
-        try {
-            mainSongsItemFormatList = new AsyncTask<Void, Void, List<MainSongsItemFormat>>() {
-                List<MainSongsItemFormat> mList = new ArrayList<>(500);
+    public void getPermission() {
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
 
-                @Override
-                protected List<MainSongsItemFormat> doInBackground(Void... params) {
-                    ContentResolver cr = getActivity().getContentResolver();
+            new AlertDialog.Builder(getContext())
+                    .setTitle("Need storage permission")
+                    .setMessage("We need to storage permission to read your storage for songs.")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(getActivity(),
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    REQUEST_WRITE_STORAGE);
 
-                    Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                    String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
-                    String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
-                    Cursor cur = cr.query(uri, null, selection, null, sortOrder);
-                    int count = 0;
-
-                    if (cur != null) {
-                        count = cur.getCount();
-
-                        if (count > 0) {
-                            while (cur.moveToNext()) {
-                                String data = cur.getString(cur.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
-                                // Add code to get more column here
-
-                                // Save to your list here
-                                mList.add(new MainSongsItemFormat(data));
-                            }
 
                         }
-                    }
+                    })
+                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
 
-                    if (cur != null) {
-                        cur.close();
-                    }
+                            android.os.Process.killProcess(android.os.Process.myPid());
+                            System.exit(1);
 
-                    return mList;
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_WRITE_STORAGE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    populateList();
+                    adapter.notifyDataSetChanged();
+
+
+                } else {
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(0);
                 }
-            }.execute().get();
-            Log.e("TAG", "size is " + mainSongsItemFormatList.size());
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
+                return;
+            }
         }
     }
+
+    private void populateList() {
+        if (mainSongsItemFormatList == null) mainSongsItemFormatList = new ArrayList<>(500);
+        else mainSongsItemFormatList.clear();
+
+        ContentResolver cr = getActivity().getContentResolver();
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
+        String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
+        Cursor cur = cr.query(uri, null, selection, null, sortOrder);
+        int count;
+
+        if (cur != null) {
+            count = cur.getCount();
+
+            if (count > 0) {
+                while (cur.moveToNext()) {
+                    String data = cur.getString(cur.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+                    // Add code to get more column here
+
+                    // Save to your list here
+                    mainSongsItemFormatList.add(new MainSongsItemFormat(data));
+                }
+
+            }
+        }
+
+        if (cur != null) {
+            cur.close();
+        }
+        adapter.notifyDataSetChanged();
+
+    }
+
+
 }
